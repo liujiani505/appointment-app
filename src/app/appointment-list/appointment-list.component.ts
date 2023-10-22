39ae448f760059c2a6724557d805e2b876985dcd
Jiani Liu <liujiani505@gmail.com>
2023-10-21 23:15:14 -0400

    created appt list component

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  appointment:string = "Take dog for a walk";
+
+}
